66b19b36c52c950524a1bfb07115376d340c8ff4
Sakingtj <nexus311051sakingtj@gmail.com>
2019-05-11 12:20:01 +0900

    fix

--- detect_move.py
+++ detect_move.py
@@ -6,6 +6,6 @@ def detect(gray,avg):
     # 動いているエリアの面積を計算してちょうどいい検知結果を抽出する
     thresh = cv2.threshold(mdframe, 3, 255, cv2.THRESH_BINARY)[1]  # 動いている部分の2値化処理
     im_mask = cv2.medianBlur(thresh, 5)  # ごま塩ノイズの除去
-    contours, hierarchy = cv2.findContours(im_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # 輪郭を検出
+    _,contours, hierarchy = cv2.findContours(im_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # 輪郭を検出
 
     return contours,hierarchy
